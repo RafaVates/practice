d203b46d0a6bde4ee420f007799520a9798e7b88
EPAM\rafael_toutain <rafael_toutain@epam.com>
2024-10-31 19:49:00 -0300

    commit desde tercera

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import random
 from first import FIRST
 from second import SECOND
+from tercera import TERCERA
 
 def main():
     x=random.randint(1,100)
     print(x)
     print(FIRST)
     print(SECOND)
+    print(TERCERA)
     
 if __name__ == "__main__":
     main()
