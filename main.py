79d43c1fd91e85514d3c28f4d13dc5b1aabc9d0f
EPAM\rafael_toutain <rafael_toutain@epam.com>
2025-05-29 10:20:32 -0300

    segundo commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 def main():
     x=random.randint(1,100)
     print(x)
-    print("Hola desde mi compu")
+    print("Hola desde mi compu, segundo commit")
         
 if __name__ == "__main__":
     main()
