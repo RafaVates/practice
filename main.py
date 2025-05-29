f44b0317f1aed5e989ffd11b2c4a23c7416dab94
RafaVates <124632257+RafaVates@users.noreply.github.com>
2025-05-29 10:25:30 -0300

    dev2
    Soy el developer 2

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     x=random.randint(1,100)
     print(x)
     print("Hola desde mi compu, segundo commit")
+    print("Hola desde otro developer")
         
 if __name__ == "__main__":
     main()
