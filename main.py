8e151835a43a3a5d8eaaa9af7bbf061db474e402
RafaVates <124632257+RafaVates@users.noreply.github.com>
2025-05-29 09:36:40 -0300

    backToOriginal
    Volviendo al repo original para practicar nuevamente

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
 import random
-from first import FIRST
-from second import SECOND
-from cuarta import CUARTA
-from tercera import TERCERA
-from quinta import QUINTA
-from sexta import SEXTA
 
 def main():
     x=random.randint(1,100)
     print(x)
-    print(FIRST)
-    print(SECOND)
-    print(CUARTA)
-    print(TERCERA)
-    print(QUINTA)
-    print(SEXTA)
-    
+        
 if __name__ == "__main__":
     main()
     
